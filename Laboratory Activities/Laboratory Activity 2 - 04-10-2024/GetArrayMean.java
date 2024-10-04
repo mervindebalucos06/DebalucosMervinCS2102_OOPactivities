d69c882dfd57e3daf3f37d46a00f3ec4c5c1a09f
Mervin <mervidebalucos@gmail.com>
2024-10-04 14:41:47 +0800

    update

--- Laboratory Activities/Laboratory Activity 2 - 04-10-2024/GetArrayMean.java
+++ Laboratory Activities/Laboratory Activity 2 - 04-10-2024/GetArrayMean.java
@@ -1,6 +1,29 @@
-
+import java.util.Scanner;
 public class GetArrayMean {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter number of elements: ");
+    int n = input.nextInt();
+
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      System.out.print("Enter number: ");
+      arr[i] = input.nextInt();
+    }
+
+    double mean = getArrayMean(arr);
+    System.out.printf("Mean of array is: %.2f%n ", mean);
     
+    input.close();
+  }
+  
+  public static double getArrayMean(int[] arr) {
+    int sum = 0;
+    for (int num : arr) {
+      sum += num;
+    }
+    return (double) sum / arr.length;
   }
 }
