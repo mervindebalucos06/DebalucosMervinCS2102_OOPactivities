d789f76d4eb5703a6eeea77159c0fe53273ce8ce
Mervin <mervidebalucos@gmail.com>
2024-11-29 00:08:36 +0800

    update

--- Laboratory Activities/Laboratory Activity 4 - 28-11-2024/Main.java
+++ Laboratory Activities/Laboratory Activity 4 - 28-11-2024/Main.java
@@ -1,10 +1,8 @@
-
-
 public class Main {
   public static void main(String[] args) {
-    Rectangle rectangle = new Rectangle(8, 12);
-    Square square = new Square(10);
-    Triangle triangle = new Triangle(10, 12, 15);
+    Rectangle rectangle = new Rectangle(7, 10);
+    Square square = new Square(9);
+    Triangle triangle = new Triangle(5, 8, 10);
 
     Shape[] shapes = { rectangle, square, triangle };
     ShapeCollection shapeCollection = new ShapeCollection(shapes);
